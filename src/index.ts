66befa9faf8789937938dd42589cf6c29305ea37
Mhvn092 <mhvn092@gmail.com>
2021-12-31 11:44:15 -0800

    second Commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { createConnection } from "typeorm";
 import express from "express";
-import { ActorEntity } from "./entities/actor-Entity";
 import { MovieEntity } from "./entities/movie-entity";
 import { GenreEntity } from "./entities/genre-entity";
 import { DirectorEntity } from "./entities/director-entity";
 import { ActorController } from "./routes/actor-controller";
 import { DirectorController } from "./routes/director-controller";
 import { MovieController } from "./routes/movie-controller";
+import { ActorEntity } from "./entities/actor-Entity";
 
 const app = express();
 
